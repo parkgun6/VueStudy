ee13ea683a30564885a9d9a57cc8956ef14f5182
parkgun6 <a01088807068@gmail.com>
2023-01-02 16:01:41 +0900

    prettier, airbnb add

--- vuetify_todo/.eslintrc.js
+++ vuetify_todo/.eslintrc.js
@@ -1,14 +1,33 @@
 module.exports = {
-  root: true,
   env: {
+    browser: true,
+    es6: true,
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
+    "eslint:recommended",
+    "prettier",
+    "airbnb",
+    "plugin:prettier/recommended",
   ],
   rules: {
-    'vue/multi-word-component-names': 'off',
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+    "react/prop-types": "off",
+    "react/function-component-definition": "off",
+    "import/no-unresolved": "off",
+    "import/prefer-default-export": "off",
+    "no-param-reassign": [
+      "error",
+      { props: true, ignorePropertyModificationsFor: ["state"] },
+    ],
   },
-}
+};
